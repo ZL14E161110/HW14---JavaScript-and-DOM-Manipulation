1685f081d38b775251df6d4c08b35ed2f2e4cde1
ZL14E161110 <34262977+ZL14E161110@users.noreply.github.com>
2018-05-20 23:28:27 -0700

    load with not sucessful pagination

--- search.js
+++ search.js
@@ -17,16 +17,16 @@ var filteredData = dataSet;
 function renderTable() {
   $tbody.innerHTML = "";
   for (var i = 0; i < filteredData.length; i++) {
-    // Get get the current sighting object and its fields
-    var sighting = filteredData[i];
-    var fields = Object.keys(sighting);
-    // Create a new row in the tbody, set the index to be i + startingIndex
+    // Get get the current ufodata object and its fields
+    var ufodata = filteredData[i];
+    var fields = Object.keys(ufodata);
+    // Create a new row in the tbody, set the index to be i and index every item inthe row
     var $row = $tbody.insertRow(i);
     for (var j = 0; j < fields.length; j++) {
-      // For every field in the sighting object, create a new cell at set its inner text to be the current value at the current sighting's field
+      // For every field in the ufodata object, create a new cell at set its inner text to be the right field
       var field = fields[j];
       var $cell = $row.insertCell(j);
-      $cell.innerText = sighting[field];
+      $cell.innerText = ufodata[field];
     }
     
   }
@@ -37,36 +37,36 @@ function handleSearchButtonClick() {
 
   var filterDate = $dateInput.value.trim();
   if (filterDate != "") {
-    filteredData = dataSet.filter(function (sighting) {
-      var sightingDate = sighting.datetime;
-      return sightingDate === filterDate;
+    filteredData = dataSet.filter(function (ufodata) {
+      var sightingDate = ufodata.datetime;
+      return sightingDate.includes(filterDate);
     });
   };
   var filterCity = $cityInput.value.trim().toLowerCase();
   if (filterCity != "") {
-    filteredData = filteredData.filter(function (sighting) {
-      var sightingCity = sighting.city;
+    filteredData = filteredData.filter(function (ufodata) {
+      var sightingCity = ufodata.city;
       return sightingCity === filterCity;
     });
   };
   var filterState = $stateInput.value.trim().toLowerCase();
   if (filterState != "") {
-    filteredData = filteredData.filter(function (sighting) {
-      var sightingState = sighting.state;
+    filteredData = filteredData.filter(function (ufodata) {
+      var sightingState = ufodata.state;
       return sightingState === filterState;
     });
   };
   var filterCountry = $countryInput.value.trim().toLowerCase();
   if (filterCountry != "") {
-    filteredData = filteredData.filter(function (sighting) {
-      var sightingCountry = sighting.country;
+    filteredData = filteredData.filter(function (ufodata) {
+      var sightingCountry = ufodata.country;
       return sightingCountry === filterCountry;
     });
   };
   var filterShape = $shapeInput.value.trim().toLowerCase();
   if (filterShape != "") {
-    filteredData = filteredData.filter(function (sighting) {
-      var sightingShape = sighting.shape;
+    filteredData = filteredData.filter(function (ufodata) {
+      var sightingShape = ufodata.shape;
       return sightingShape === filterShape;
     });
   };
